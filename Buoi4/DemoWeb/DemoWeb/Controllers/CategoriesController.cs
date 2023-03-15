2113c1dde667c2dab2302f1067a7b5e1adc893c3
Nguyễn Thị Diễm Khang <diemkhang1912@gmail.com>
2023-03-15 09:21:13 +0700

    buoi6_15/3/2023

---
+++ Buoi4/DemoWeb/DemoWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoWeb.Models;
+
+namespace DemoWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        DBSportStoreEntities database = new DBSportStoreEntities();
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var categories = database.Categories.ToList();
+            return View(categories);
+        }
+        
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            try
+            {
+                database.Categories.Add(category);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("Lỗi tạo mới CATEGORY!");
+            }
+        }
+    }
+}
