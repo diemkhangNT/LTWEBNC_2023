dc176961fda93f539b895424d445f0bdbaf74c21
Nguyễn Thị Diễm Khang <diemkhang1912@gmail.com>
2023-04-13 08:24:12 +0700

    buoi10

---
+++ Buoi4/DemoWeb/DemoWeb/Controllers/CustomerProductsController.cs
@@ -0,0 +1,39 @@
+﻿using DemoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
+
+namespace DemoWeb.Controllers
+{
+    public class CustomerProductsController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products;
+            return View(products.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new
+               HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+    }
+
+}
