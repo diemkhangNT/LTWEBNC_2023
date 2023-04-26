5a533d042df9b5db364cc46bc385c4d3a52e5eb7
Nguyễn Thị Diễm Khang <diemkhang1912@gmail.com>
2023-04-26 12:56:09 +0700

    Buoi12

--- Buoi4/DemoWeb/DemoWeb/Controllers/CustomerProductsController.cs
+++ Buoi4/DemoWeb/DemoWeb/Controllers/CustomerProductsController.cs
@@ -34,6 +34,17 @@ namespace DemoWeb.Controllers
             return View(product);
         }
 
+        public ActionResult GetProductsByCategory()
+        {
+            var categories = db.Categories.ToList();
+            return PartialView("CategoriesPartialView", categories);
+        }
+
+        public ActionResult GetProductsByCateId(int id)
+        {
+            var products = db.Products.Where(p => p.Category1.Id == id).ToList();
+            return View("Index", products);
+        }
     }
 
 }
